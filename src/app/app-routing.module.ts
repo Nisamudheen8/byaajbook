f56da71294679e871607a83ab9444ab1b57625df
nizam <nizambinnazar@gmail.com>
2022-06-04 00:24:39 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { OurProductsComponent } from './our-products/our-products.component';
+import { PricingComponent } from './pricing/pricing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'footer',component:HomeComponent
+  },
+  {
+    path:'our-products',component:OurProductsComponent
+  },
+  {
+    path:'pricing',component:PricingComponent
+  },
+  {
+    path:'about',component:AboutComponent
+  },
+  {
+    path:'contact-us',component:ContactUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { OurProductsComponent } from './our-products/our-products.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    OurProductsComponent,
+    PricingComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
